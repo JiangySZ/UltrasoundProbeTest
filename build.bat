00dcceaf3a11f3eabb7809753cdffdd48bc5e61b
unknown <80008@wisonic.cn>
2026-03-05 18:05:37 +0800

    project-skeleton: 添加编译后自动运行功能

--- build.bat
+++ build.bat
@@ -1,11 +1,17 @@
 @echo off
-set QMAKESPEC=win32-msvc
 set QTDIR=C:\Qt\Qt5.9.1\5.9.1\msvc2017_64
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 cd /d G:\AI_WORk\Sys_Prb_Test
 
-"%QTDIR%\bin\qmake.exe" UltrasoundProbeTest.pro -spec %QMAKESPEC% CONFIG+=debug
+"%QTDIR%\bin\qmake.exe" UltrasoundProbeTest.pro -spec win32-msvc CONFIG+=debug
 
 nmake -f Makefile.Debug
+
+echo.
+echo Build completed!
+echo Setting Qt PATH and running...
+set PATH=%QTDIR%\bin;%PATH%
+
+debug\UltrasoundProbeTest.exe
